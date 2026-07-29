6b251d013e3a7ef8daeb519e16d7b1d11c8c6480
Shaik Anas <shaikanas@Shaiks-MacBook-Air-3.local>
2026-07-29 23:30:12 +0530

    Add club images

--- client/src/components/ClubCard.js
+++ client/src/components/ClubCard.js
@@ -1,94 +1,54 @@
 import { Link } from "react-router-dom";
 import "./ClubCard.css";
 
-
 function ClubCard({
+  image,
+  title,
+  description,
+  id
+}) {
 
-image,
+  return (
 
-title,
+    <div className="card shadow h-100">
 
-description,
+      <img
+        src={
+          image
+            ? `https://club-recruitment.onrender.com/uploads/${image}`
+            : "https://via.placeholder.com/300"
+        }
+        alt={title}
+        className="card-img-top"
+        style={{
+          height: "220px",
+          objectFit: "cover"
+        }}
+      />
 
-id
+      <div className="card-body">
 
-}){
+        <h3>
+          {title}
+        </h3>
 
+        <p>
+          {description}
+        </p>
 
-return(
+        <Link
+          to={`/club/${id}`}
+          className="btn btn-primary"
+        >
+          View Club
+        </Link>
 
+      </div>
 
-<div className="card shadow h-100">
+    </div>
 
-
-<img
-
-src={image}
-
-alt={title}
-
-className="card-img-top"
-
-style={{
-
-height:"220px",
-
-objectFit:"cover"
-
-}}
-
-/>
-
-
-
-
-
-<div className="card-body">
-
-
-<h3>
-
-{title}
-
-</h3>
-
-
-
-<p>
-
-{description}
-
-</p>
-
-
-
-
-
-<Link
-
-to={`/club/${id}`}
-
-className="btn btn-primary"
-
->
-
-View Club
-
-</Link>
-
-
-
-</div>
-
-
-</div>
-
-
-
-);
-
+  );
 
 }
-
 
 export default ClubCard;
